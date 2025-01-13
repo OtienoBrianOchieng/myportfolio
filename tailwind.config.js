cf55349e8517134be3288552d1d4821adcd10f27
Brian Ochieng <brianochieng1989@gmail.com>
2025-01-13 22:10:55 +0300

    setting portfolio environment

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html',  // if you have an index.html
+    './src/**/*.{js,jsx,ts,tsx}',  // include JS, JSX, TS, and TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
